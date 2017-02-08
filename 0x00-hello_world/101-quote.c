25b6b34e03714a0cc143190ac7cf04b3a5c30d7e
jarehec <jarehec@github.com>
2017-02-08 22:27:55 +0000

    removed printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 *Return: 1
 */
 int main(void)
-{	
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
