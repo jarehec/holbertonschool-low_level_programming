cee6bddbd8b37dce8521a25b8e5a91ce8ea00500
jarehec <jarehec@github.com>
2017-02-14 19:06:14 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* add - adds two ints
+* @a: first int
+* @b: second int
+* Return: a + b
+*/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
