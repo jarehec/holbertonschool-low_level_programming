2a2ca81eb09eb7dc3a36a4ae70169328ce83ecb8
jarehec <jarehec@github.com>
2017-02-15 01:44:30 +0000

    fixed output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,31 +6,31 @@
 */
 void print_to_98(int n)
 {
-	if (n != 98)
+	if (n == 98)
+		printf("%d\n", n);
+	else if (n != 98)
 	{
-		if (n >= 98)
+		while (n <= 98)
 		{
-			while (n >= 98)
+			if (n != 98)
+				printf("%d, ", n);
+			n++;
+			if (n == 98)
 			{
-				if (n != 98)
-					printf("%d, ", n);
-				n--;
-				if (n == 98)
-					break;
+				printf("%d\n", n);
+				break;
 			}
 		}
-		if (n <= 98)
+		while (n >= 98)
 		{
-			while (n <= 98)
+			if (n != 98)
+				printf("%d, ", n);
+			n--;
+			if (n == 98)
 			{
-				if (n != 98)
-					printf("%d, ", n);
-				n++;
-				if (n == 98)
-					break;
+				printf("%d\n", n);
+				break;
 			}
 		}
 	}
-	if (n == 98)
-		printf("%d\n", n);
 }
