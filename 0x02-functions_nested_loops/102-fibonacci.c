8ce331fe5f9b65a0706612e9a06d94684216f029
jarehec <jarehec@github.com>
2017-02-16 06:21:37 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints first 50 fibo nums
+* Returm: 0
+*/
+int main(void)
+{
+	unsigned long int left = 1, right = 1, count;
+
+	for (count = 1; count <= 50; count++)
+	{
+		right += left;
+
+		printf("%lu", left);
+		if (count <= 48)
+		{
+			printf(", ");
+			printf("%lu, ", right);
+		}
+		count++;
+		left += right;
+	}
+	putchar('\n');
+	return (0);
+}
