3ef24e383be826dd5e8a04c9552afa06ba96af16
jarehec <jarehec@github.com>
2017-02-21 22:58:45 +0000

    intial commit

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* puts_half - prints the second half of a string
+* @str: input string
+*/
+void puts_half(char *str)
+{
+	unsigned int count, hf;
+
+	for (count = 0; str[count] != '\0'; count++)
+	;
+	if ((count % 2) != 0)
+		count -= 1;
+
+	hf = count /= 2;
+
+	for (count = hf; str[count] != '\0'; count++)
+		_putchar(str[count]);
+
+	_putchar('\n');
+}
