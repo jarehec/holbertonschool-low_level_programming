91845063a9ddc49b4f6fc1e927ebd295a3443968
jarehec <jarehec@github.com>
2017-02-22 05:59:41 +0000

    check for 0

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 /**
 * print_array - prints n elements of an array of integers
-* @a: input
+* @a: input array
 * @n: number of elements
 */
 void print_array(int *a, int n)
 {
 	int count;
-
-	for (count = 0; count < n; count++)
+	
+	if (n > 0)
 	{
-		if (count + 1 < n)
-			printf("%d, ", a[count]);
-		if (count + 1 == n)
-			printf("%d\n", a[count]);
+		for (count = 0; count < n; count++)
+		{
+			if (count + 1 < n)
+				printf("%d, ", a[count]);
+			if (count + 1 == n)
+				printf("%d\n", a[count]);
+		}
 	}
 }
