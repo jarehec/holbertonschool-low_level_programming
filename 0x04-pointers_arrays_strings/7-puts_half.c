55738759de2d5d7cab3610b182de6a2c4dc445f0
jarehec <jarehec@github.com>
2017-02-22 23:44:45 +0000

    fixed output

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,14 @@
 */
 void puts_half(char *str)
 {
-	int count, loc;
+	unsigned int count, loc;
 
 	for (count = 0; str[count] != '\0'; count++)
 	;
 
 	for (loc = 0; loc < count; loc++)
 	{
-		if (loc >= count / 2)
+		if (loc > count / 2)
 		_putchar(str[loc]);
 	}
 	_putchar('\n');
