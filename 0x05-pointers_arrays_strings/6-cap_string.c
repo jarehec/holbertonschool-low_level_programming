d50fcb61587061c27ef52dac723ab22e1018b9ef
jarehec <jarehec@github.com>
2017-02-24 07:22:18 +0000

    check chars first

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,9 @@ char *cap_string(char *str)
 {
 	int i = 0;
 ;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') &&
-		(*(str + i - 1) == ',' ||
+		if ((*(str + i - 1) == ',' ||
 		*(str + i - 1) == ';' ||
 		*(str + i - 1) == '.' ||
 		*(str + i - 1) == '!' ||
@@ -24,8 +23,8 @@ char *cap_string(char *str)
 		*(str + i - 1) == ' ' ||
 		*(str + i - 1) == '\n' ||
 		*(str + i - 1) == '\t'))
-			str[i] -= 32;
-		i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
 	}
 	return (str);
 }
