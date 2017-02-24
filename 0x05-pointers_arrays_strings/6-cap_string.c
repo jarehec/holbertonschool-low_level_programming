8f9faf2746f7b0d598124506c7d9d76a50a74f4f
jarehec <jarehec@github.com>
2017-02-24 07:58:45 +0000

    test str[0]

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -7,9 +7,11 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] -= 32;
 		if ((*(str + i - 1) == ',' ||
 		*(str + i - 1) == ';' ||
 		*(str + i - 1) == '.' ||
