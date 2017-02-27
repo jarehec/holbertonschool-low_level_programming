6a94769031f3f57a43e3e33e790d46d3051d533c
jarehec <jarehec@github.com>
2017-02-27 20:09:31 +0000

    changed return

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count;
-	char *sptr = s;
+	char **sptr = &s;
 
 	for (count = 0; count < n; count++)
 		*(s + count) += b;
 
-	return (sptr);
+	return (*sptr);
 }
