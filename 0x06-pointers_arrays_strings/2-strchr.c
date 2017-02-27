9129d3fa52cf2972375daf6ccc0fdffc059def74
jarehec <jarehec@github.com>
2017-02-27 23:18:51 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*_strchr - locates a character in a string
+* @s: string
+* @c: character
+* Return: pointer to first occurence of c
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int fnd;
+
+	for (fnd = 0; s[fnd] != '\0'; fnd++)
+	{
+		if (s[fnd] == c)
+		{
+			s = &s[fnd];
+			return (s);
+		}
+	}
+	return (0);
+}
