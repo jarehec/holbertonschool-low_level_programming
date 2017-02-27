76f8b15290328e09468f2dc02264eaaf739e8ed1
jarehec <jarehec@github.com>
2017-02-27 23:56:57 +0000

    test

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
 */
 char *_strchr(char *s, char c)
 {
-	int fnd;
+	unsigned int fnd;
 
 	for (fnd = 0; s[fnd] != '\0'; fnd++)
 	{
@@ -17,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		}
 	}
-	return ("NULL");
+	return ('\0');
 }
