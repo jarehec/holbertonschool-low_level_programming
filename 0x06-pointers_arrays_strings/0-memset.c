09a983c082d6955c0740e8cf5305c4c6c59bb790
jarehec <jarehec@github.com>
2017-02-27 10:23:32 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _memset - fills memory with a constant byte
+* @s: memory area
+* @b: constant byte
+* @n: number of bytes
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+		*(s + count) += b;
+
+	return (0);
+}
