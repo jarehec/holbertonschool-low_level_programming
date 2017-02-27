0c9cf6d123d6ccd2d3216af2604c9eb097bcd35d
jarehec <jarehec@github.com>
2017-02-27 22:37:21 +0000

    < not <=

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int count;
 
 	for (count = 0; count < n; count++)
-		dest[count] += src[count];
+		dest[count] = src[count];
 
 	return (dest);
 }
