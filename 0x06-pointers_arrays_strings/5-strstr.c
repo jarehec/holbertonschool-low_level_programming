218721cb9575a10359f1a6cccb4462486909c71d
jarehec <jarehec@github.com>
2017-02-28 22:44:02 +0000

    fixed style

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -9,18 +9,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int ct = 0, st, i = 0;
 
-	 if (needle[0] == '\0')
+	if (needle[0] == '\0')
 		return (haystack);
 
 	while (haystack[i] != '\0')
 	{
-			st = i;
+		st = i;
 
 		for (ct = 0; haystack[i] == needle[ct] && haystack[i] != '\0'; ct++)
 		{
 			if (needle[ct + 1] == '\0')
 				return (haystack + st);
-			
 			i++;
 		}
 		i = st + 1;
