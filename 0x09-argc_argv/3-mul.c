7184189267f774cdce88a2581f729650ec8c1bd5
jarehec <jarehec@github.com>
2017-03-05 13:49:46 +0000

    initial commit

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: argument
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, mult = 1;
+
+	if (argc <= 3)
+	{
+		for (i = 1; i <= 2; i++)
+			mult *= atoi(argv[i]);
+		printf("%d\n", mult);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
