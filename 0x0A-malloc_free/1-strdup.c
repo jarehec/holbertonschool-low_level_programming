423868c5eb581ac66a640c61a091d361f5ea1590
jarehec <bone1198@gmail.com>
2017-03-07 20:16:13 +0000

    initial commit

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - returns a pointer to memory with a copy with a copy of the string
+* @str: string
+* Return: mem
+*/
+char *_strdup(char *str)
+{
+	int i, len;
+	char *mem;
+
+	for (len = 0; str[len] != '\0'; len++)
+	;
+
+	mem = malloc(sizeof(char) * len);
+	for (i = 0; i <= len; i++)
+		mem[i] = str[i];
+
+	return (mem);
+}
