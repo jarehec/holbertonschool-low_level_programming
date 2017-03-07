abd54971a94740442f35642dc77414ceb777e2e1
jarehec <bone1198@gmail.com>
2017-03-07 17:47:53 +0000

    initial commit

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* create_array - creates an array and sets it to char
+* @size: size of array
+* @c: initial value
+* Return: mem
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *mem;
+	unsigned int i;
+
+	mem = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		mem[i] = c;
+
+	return (mem);
+}
