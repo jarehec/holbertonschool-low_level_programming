7d88ffe4724b5285625b6793d47dfca94856a5c0
jarehec <bone1198@gmail.com>
2017-03-07 20:33:04 +0000

    typecast

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -11,8 +11,8 @@ char *create_array(unsigned int size, char c)
 	char *mem;
 	unsigned int i;
 
-	mem = malloc(sizeof(char) * size);
-	for (i = 0; i <= size; i++)
+	mem = (char *)malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
 		mem[i] = c;
 
 	return (mem);
