bbb4fd4aad140cd8048d09ad834baad87b5b0030
jarehec <bone1198@gmail.com>
2017-03-07 23:28:02 +0000

    test

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	for (len1 = 0; s2[len1] != '\0'; len1++)
 	;
 
-	mem = malloc(sizeof(char) * (len + len1 + 1));
+	mem = malloc(sizeof(char) * (len + len1 + 2));
 
 	for (i = 0; i <= len; i++)
 		mem[i] = s1[i];
