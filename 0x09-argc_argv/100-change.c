ae265d67aaa49ae79f36f15a12369ae1bc48b9cc
jarehec <bone1198@gmail.com>
2017-03-07 06:41:25 +0000

    initial commit

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - calculates change
+* @argc: number of arguments
+* @argv: argument
+* Return: 0 or 1 if error
+*/
+int main(int argc, char *argv[])
+{
+	int i, chg = 0, money[5] = {25, 10, 5, 2, 1};
+	int amt = atoi(argv[1]);
+
+	if (argc == 2 && amt >= 0)
+	{
+		for (i = 0; amt != 0; i++)
+		{
+			chg += amt / money[i];
+			amt %= money[i];
+		}
+			printf("%d\n", chg);
+	}
+	else
+	{
+		puts("Error");
+		return (1);
+	}
+
+	return (0);
+}
