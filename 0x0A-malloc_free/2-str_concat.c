fe2490d4947d4aa521412fbcd87cfd6b83aa81fc
jarehec <bone1198@gmail.com>
2017-03-08 03:22:39 +0000

    revert

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,10 +13,6 @@ char *str_concat(char *s1, char *s2)
 
 	if (*s1 == '\0' && *s2 == '\0')
 		return (NULL);
-	if (*s1 == '\0')
-		s1 = "";
-	if (*s1 == '\0')
-		s2 = "";
 
 	for (len = 0; s1[len] != '\0'; len++)
 	;
