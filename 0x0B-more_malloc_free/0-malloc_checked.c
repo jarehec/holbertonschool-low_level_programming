f64472dd9984e1560fc0d727102fc6854ab82020
jarehec <bone1198@gmail.com>
2017-03-09 20:22:50 +0000

    initial commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* malloc_checked - allocates memory
+* @b: bytes
+* Return: pointer to memory or NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *mem = malloc(sizeof(b) * b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
