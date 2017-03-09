bb9febac00fb1a9347dc09ace2786ef17e9b15d2
jarehec <bone1198@gmail.com>
2017-03-09 22:06:36 +0000

    fixed

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = malloc(size);
+	ar = malloc(nmemb * size);
 
 	if (ar == NULL)
 		return (NULL);
