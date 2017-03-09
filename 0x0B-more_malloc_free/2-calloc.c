85ba56582095394606d5f78a6c4de2a6d2e89ba8
jarehec <bone1198@gmail.com>
2017-03-09 21:46:35 +0000

    fixed sizeof parameter

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = malloc(nmemb * sizeof(int));
+	ar = malloc(nmemb * sizeof(size));
 
 	if (ar == NULL)
 		return (NULL);
