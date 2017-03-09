cfad9e712ad493f3e86a01c7539661e2f191d27b
jarehec <bone1198@gmail.com>
2017-03-09 06:25:34 +0000

    added null char

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -25,7 +25,7 @@ char *argstostr(int ac, char **av)
 
 	if (cpy == NULL)
 		return (NULL);
-
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++, k++)
 		{
@@ -33,5 +33,6 @@ char *argstostr(int ac, char **av)
 		}
 		cpy[k++] = '\n';
 	}
+	cpy[k] = '\0';
 	return (cpy);
 }
