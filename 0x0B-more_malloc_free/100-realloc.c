35d1657418a97a74878c11fdfdcfe770196c0c97
jarehec <bone1198@gmail.com>
2017-03-10 07:48:19 +0000

    return ptr

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	new = malloc(new_size + old_size);
 	if (new == NULL)
-		return (NULL);
+		return (ptr);
 
 	return (new);
 }
