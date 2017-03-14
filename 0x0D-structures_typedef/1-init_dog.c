8600477c22b53ff1c11eeb3a29277d11ff20fe22
jarehec <bone1198@gmail.com>
2017-03-14 00:59:33 +0000

    initial commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include"dog.h"
+/**
+* init_dog - intializes a variable of type dog
+* @d: struct
+* @name: val of name
+* @age: val of age
+* @owner: owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
