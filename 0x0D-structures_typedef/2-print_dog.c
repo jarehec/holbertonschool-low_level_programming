848418d7eefcc11269c7e6eae01bb00ed51d45a3
jarehec <bone1198@gmail.com>
2017-03-14 01:33:10 +0000

    initial commit

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* print_dog - prints elements from struct dog
+* @d: dog struct
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)\n";
+	if (d->owner == NULL)
+		d->owner = "(nil)\n";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
