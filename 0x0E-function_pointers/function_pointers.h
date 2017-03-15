60cca8ddb6d2e2a31fe0be979f162389309d687f
jarehec <bone1198@gmail.com>
2017-03-15 18:24:34 +0000

    check function null

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -2,7 +2,6 @@
 #define HOLBERTON_H
 typedef unsigned long int size_t;
 void print_name(char *name, void (*f)(char *));
-int _isdigit(char *c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
