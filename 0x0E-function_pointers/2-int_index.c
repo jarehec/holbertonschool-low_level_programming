bad48789d198f3258f0a3127329ca340a3c4cf92
jarehec <bone1198@gmail.com>
2017-03-15 18:29:23 +0000

    check for null

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
 * int_index - searches for an integer
 * @array: array to search
@@ -9,6 +10,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL || size == 0 || cmp == NULL)
+		return (0);
 
 	if (size <= 0)
 		return (-1);
