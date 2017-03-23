f70106d0c91719fcb8a02b7709dca78f5acbf457
jarehec <bone1198@gmail.com>
2017-03-23 22:12:14 +0000

    get the strlen of the element instead of the string

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -11,17 +11,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new  = NULL;
-	
-	new = malloc(sizeof(list_t));
 
+	new = malloc(sizeof(list_t));
 	if (new != NULL)
 	{
 		new->str = strdup(str);
-		new->len = strlen(str);
+		new->len = strlen(new->str);
 		new->next = *head;
 		*head = new;
 	}
 	else
 		return (NULL);
+
 	return (*head);
 }
