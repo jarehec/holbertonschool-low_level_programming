999d126e6b6b1c15ee0df16017d991d595fb760b
jarehec <bone1198@gmail.com>
2017-03-23 20:06:18 +0000

    check for null, added stdlib.h

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
 * list_len - counts the elements in a linked list
 * @h: linked list
@@ -9,9 +10,10 @@ size_t list_len(const list_t *h)
 {
 	size_t i;
 
+	if (h == NULL)
+		return (NULL);
 	for (i = 0; h != NULL; i++)
-	{
 		h = h->next;
-	}
+
 	return (i);
 }
