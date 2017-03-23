8c7a226714b629bb0c533b7194add8c56bd8fb91
jarehec <bone1198@gmail.com>
2017-03-23 21:58:26 +0000

    initialized new at NULL, moved head assignment inside brackets

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -10,17 +10,18 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new  = malloc(sizeof(list_t));
+	list_t *new  = NULL;
+	
+	new = malloc(sizeof(list_t));
 
 	if (new != NULL)
 	{
 		new->str = strdup(str);
 		new->len = strlen(str);
 		new->next = *head;
+		*head = new;
 	}
 	else
 		return (NULL);
-
-	*head = new;
 	return (*head);
 }
