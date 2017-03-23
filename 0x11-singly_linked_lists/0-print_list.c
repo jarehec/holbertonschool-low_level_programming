e6df9cf1e1455cd077c9a4e174aeb6439215baf6
jarehec <bone1198@gmail.com>
2017-03-23 20:00:05 +0000

    added stdlib.h, check for null

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
 * print_list - prints the elements of a linked list
@@ -10,6 +11,8 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
+	if (h == NULL)
+		return (0);
 	for (i = 0; h != NULL; i++)
 	{
 		printf("[%u] %s\n", h->len, h->str);
