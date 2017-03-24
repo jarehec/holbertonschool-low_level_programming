65e11537d14b709a481e1305f16af05af7cd7195
jarehec <bone1198@gmail.com>
2017-03-24 19:19:51 +0000

    added destrcutor, put string on two lines

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,9 +1,14 @@
+#include "lists.h"
 #include <stdio.h>
 /**
 * first - prints a string before main
-*
 */
-void __attribute__((constructor)) first(void)
+void first(void) __attribute__((constructor));
+void clean(void)__attribute__((destructor));
+
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+void clean(void);
