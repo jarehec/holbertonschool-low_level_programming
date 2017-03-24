278c4366603411e3b81c81d31aa395562471caa7
jarehec <bone1198@gmail.com>
2017-03-24 08:29:46 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* free_list - frees a list_t list 
+* @head: linked list
+*/
+void free_list(list_t *head)
+{
+	size_t i;
+
+	for (i = 0; head != NULL; i++)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+	free(head);
+}
