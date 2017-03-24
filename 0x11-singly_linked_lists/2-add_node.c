b7a6cee19ffea3700ce1bf319464bf811d4ef0f3
jarehec <bone1198@gmail.com>
2017-03-24 04:39:22 +0000

    replaced standard strlen with my strlen

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -11,17 +11,31 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new  = NULL;
-	char *cp_str = strdup(str);
-	unsigned int len = strlen(cp_str);
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (new != NULL)
+	{
+		new->str = strdup(str);
+		new->len = _strlen(new->str);
+		new->next = *head;
+		*head = new;
+	}
+	else
 		return (NULL);
 
-	new->str = cp_str;
-	new->len = len;
-	new->next = *head;
-	*head = new;
-
 	return (new);
+}
+/**
+* _strlen - returns the length of a string
+* @s: string input
+* Return: length of s
+*/
+int _strlen(char *s)
+{
+	unsigned int lngth = 0;
+
+	for (lngth = 0; s[lngth] != '\0'; lngth++)
+	;
+
+	return (lngth);
 }
