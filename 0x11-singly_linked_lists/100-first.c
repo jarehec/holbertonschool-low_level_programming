b6fe0cd5262f7f6f4c62172c47fd21b5b0fe90bc
jarehec <bone1198@gmail.com>
2017-03-24 18:55:12 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* first - prints a string before main
+*
+*/
+void __attribute__((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
