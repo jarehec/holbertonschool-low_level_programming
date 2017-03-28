48bcc6433de131e14a8f8d5b047b069a3b9d8a82
jarehec <bone1198@gmail.com>
2017-03-28 17:18:09 +0000

    check if node is null

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head != NULL)
 	{
-		for (i = 0; i < index; i++)
+		for (i = 0; i < index && node != NULL; i++)
 		{
 			temp = node;
 			node = node->next;
