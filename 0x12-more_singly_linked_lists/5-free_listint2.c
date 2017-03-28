ff950bb6d8d33658d35862e387dd309ac5f605e2
jarehec <bone1198@gmail.com>
2017-03-28 19:57:42 +0000

    compare head to null

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		(*head) = temp;
 	}
-	head = NULL;
 }
