37c76a055ef6c6b55b636564a6b92ff08e8b4a5f
jarehec <bone1198@gmail.com>
2017-03-28 19:50:04 +0000

    added parenthesis

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,8 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		temp = (*head)->next;
+		(*head) = temp;
+		free(*head);
 	}
-	*head = NULL;
 }
