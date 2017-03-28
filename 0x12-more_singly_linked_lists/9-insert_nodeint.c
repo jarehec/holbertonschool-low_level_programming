1b08f35992e842b89eaca8c99edf53e96846fc70
jarehec <bone1198@gmail.com>
2017-03-28 17:40:47 +0000

    free new node if failed

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				temp->next = new;
 			}
 			else
+			{
+				free(new);
 				return (NULL);
+			}
 		}
 	}
 	return (new);
