26c302e150b519983f89e6997b90a45d9eb4684b
jarehec <bone1198@gmail.com>
2017-03-28 17:32:50 +0000

    returns NULL if temp is null

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->n = n;
 			new->next = NULL;
-	
 			for (i = 0; i < idx - 1 && temp != NULL; i++)
 				temp = temp->next;
 			if (temp)
@@ -28,6 +27,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				new->next = temp->next;
 				temp->next = new;
 			}
+			else
+				return (NULL);
 		}
 	}
 	return (new);
