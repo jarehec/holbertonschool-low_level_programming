b542e682f668e359a6c5d7d5a986e814219188bf
jarehec <bone1198@gmail.com>
2017-03-30 21:51:17 +0000

    rewrite using bitwise operators

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -7,71 +7,15 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, idx, dec = 0;
-	char set = 1;
+	unsigned int i, flip = 1, num = 0;
 
-	if (b == NULL)
-		return (0);
-
-	len = checkbin(b);
-
-	if (len != 0)
+	for (i = 0; b && b[i] != '\0'; i++)
 	{
-		for (idx = 0; b[idx] != '\0'; idx++)
-		{
-			if (b[idx] == '1')
-				set = 0;
-			if (set == 0)
-			{
-				dec += (b[idx] - 48) * _pow(2, len);
-				len--;
-			}
-		}
-	}
-	return (dec / 2);
-}
-/**
-* checkbin - checks if num is bin
-* @str: num string
-*
-* Return: len of binary
-*/
-unsigned int checkbin(const char *str)
-{
-	unsigned int idx, len = 0;
-	char set = 1;
-
-	if (str == NULL)
-		return (0);
-
-	for (idx = 0; str[idx] != '\0'; idx++)
-	{
-		if (str[idx] == '1')
-			set = 0;
-		if (set == 0)
-			len++;
-		if (str[idx] != '0' && str[idx] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		num <<= 1;
+		if (b[i] == '1')
+			num ^= flip;
 	}
-	return (len);
-}
-/**
-* _pow - pow implementation
-* @base: base
-* @exp: exponent
-*
-* Return: base ^ exp
-*/
-long int _pow(unsigned int base, unsigned int exp)
-{
-	int res = 1;
-
-	while (exp)
-	{
-		if (exp & 1)
-			res *= base;
-		exp >>= 1;
-		base *= base;
-	}
-	return (res);
+	return (num);
 }
