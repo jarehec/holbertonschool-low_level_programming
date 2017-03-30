7971a6360f9cd2cd11aa7a327fdd52e3289b3f83
jarehec <bone1198@gmail.com>
2017-03-30 19:43:33 +0000

    initial commit

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,71 @@
+#include "holberton.h"
+/**
+* binary_to_uint - converts binary to base10
+* @b: binary digit
+*
+* Return: decimal value or 0
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len, idx, dec = 0;
+	char set = 1;
+
+	len = checkbin(b);
+
+	if (b == NULL || len == 0)
+		return (0);
+
+	for (idx = 0; b[idx] != '\0'; idx++)
+	{
+		if (b[idx] == '1')
+			set = 0;
+		if (set == 0)
+		{
+			dec += (b[idx] - 48) * _pow(2, len);
+			len--;
+		}
+	}
+	return (dec / 2);
+}
+/**
+* checkbin - checks if num is bin
+* @str: num string
+*
+* Return: len of binary
+*/
+unsigned int checkbin(const char *str)
+{
+	unsigned int idx, len = 0;
+	char set = 1;
+
+	for (idx = 0; str[idx] != '\0'; idx++)
+	{
+		if (str[idx] == '1')
+			set = 0;
+		if (set == 0)
+			len++;
+		if (str[idx] != '0' && str[idx] != '1')
+			return (0);
+	}
+	return (len);
+}
+/**
+* _pow - pow implementation
+* @base: base
+* @exp: exponent
+*
+* Return: base ^ exp
+*/
+int _pow(unsigned int base, unsigned int exp)
+{
+	int res = 1;
+
+	while (exp)
+	{
+		if (exp & 1)
+			res *= base;
+		exp >>= 1;
+		base *= base;
+	}
+	return (res);
+}
---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,44 @@
+## Bit manipulation
+* 0-binary_to_uint.c
+* 1-print_binary.c
+* 2-get_bit.c
+* 3-set_bit.c
+* 4-clear_bit.c
+* 5-flip_bits.c
+### Usage
+
+```
+```
+```
+
+                  `.`
+             `-/++o:
+            `++o++o
+            :+++++o:`
+          -/++++++++++:.
+        .sy+++ooo+++++++/`
+       -yyo++:oyyso+++++++`
+      .yys++- `syyo+++++++:
+   `./sso+/-  .yys++++++++/
+`:/++/-.....-/ooo+++++++++/
+ ``.` `:+ossso++++++++++++`
+    `/sssssoo++++++++++++/`
+   -sysssso++++++++++++++++.
+  .yysssso++++++++++++++o+++.
+  oysssso++++++++:-.````.:+::
+  yyssss+++++++-`           `
+  yyssss++++++.`
+  +ysssso+++++++/:`
+  `syssss+++++++++/
+   .syssso+++++++++`
+    `+ysssso++++++:
+      -syssssoo+:.
+       `:ssssss+.
+          -+ossss/`
+            `:+ssso`
+           .ooo//ss/
+          ::y+o+`ss+
+          +o+///yss-
+           /syyyso-
+             `..`
+```
