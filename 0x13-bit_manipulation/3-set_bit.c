6a9bfd626cb070803ead7f57558f2ab633aca171
jarehec <bone1198@gmail.com>
2017-03-31 06:17:57 +0000

    revert

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,9 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 31)
 		return (-1);
 
-	*n |= 1 << index;
-
-	if (*n & 1)
+	*n |= (1 << index);
+	if (n[index] & 1)
 		return (1);
 	else
 		return (-1);
