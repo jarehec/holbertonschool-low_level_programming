89f2a63e4b57d932612425198ffa3dfe5cffead7
jarehec <bone1198@gmail.com>
2017-03-31 18:06:48 +0000

    check if operation failed

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -8,9 +8,11 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 31 || n == NULL)
+	unsigned int max = sizeof(n) * 8 - 1;
+
+	if (index > max || n == NULL)
 		return (-1);
 
 	*n |= (1 << index);
-	return (*n);
+	return ((*n == 1) ? 1 : -1);
 }
