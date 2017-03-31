c06f3b1e19652f2743787ebb9e2a666a12645b6f
jarehec <bone1198@gmail.com>
2017-03-31 20:18:41 +0000

    using sizeof

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -4,14 +4,16 @@
 * @n: base10 integer
 * @index: index
 *
-* Return: 0 or 1, -1 if error
+* Return: 1, -1 if error
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 31 || n == NULL)
+	if (index > (sizeof(*n) * 8 - 1) || n == NULL)
 		return (-1);
 
 	*n |= (1 << index);
-
-	return (*n & 1 ? 1 : -1);
+	if (*n & 1)
+		return (1);
+	else
+		return (-1);
 }
