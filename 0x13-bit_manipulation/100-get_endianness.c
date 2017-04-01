f56aa3b282db94e1beafc91a441d634287690bf9
jarehec <bone1198@gmail.com>
2017-04-01 06:55:26 +0000

    added prototype and 100

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* get_endianness - gets the endianness of the machine
+*
+* Return: 0 if little endian, 1 if not
+*/
+int get_endianness(void)
+{
+	int n = 1;
+
+	return (*(char *)&n == 1 ? 0 : 1);
+}
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -6,6 +6,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char);
 #endif
