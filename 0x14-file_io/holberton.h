78eb81bd4bd39fdc4d24a8366b1cc164b1e1f33b
jarehec <bone1198@gmail.com>
2017-04-04 22:17:05 +0000

    initial commit

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -8,5 +8,5 @@
 typedef unsigned long int size_t;
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-size_t _size(const char *file);
+ssize_t _strlen(char *s);
 #endif
