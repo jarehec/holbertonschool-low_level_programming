0599be1379a8b10152cd3182f9eaaacc741c2c76
jarehec <bone1198@gmail.com>
2017-05-08 07:15:38 +0000

    check if h is null

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		for (; idx > 0 && (*h); idx--)
 			(*h) = (*h)->next;
-		if (idx != 0)
+		if (!*h)
 		{
 			free(new);
 			return (NULL);
