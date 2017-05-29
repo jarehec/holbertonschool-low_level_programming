d49b35c70b001e25f20ddf674d2747677ba92541
jarehec <bone1198@gmail.com>
2017-05-29 23:36:02 +0000

    initial commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates an empty hash table
+ * @size: size of the hash table node
+ * Return: pointer to hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+
+	table = malloc(sizeof(hash_table_t));
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table || !table->array)
+		return (NULL);
+	table->size = size;
+	return (table);
+}
