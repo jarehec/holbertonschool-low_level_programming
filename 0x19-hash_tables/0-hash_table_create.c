0a3b5fc6a877828ba15c143bbefec6e2a9093401
jarehec <bone1198@gmail.com>
2017-05-31 20:37:04 +0000

    put 0 instead of null

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
