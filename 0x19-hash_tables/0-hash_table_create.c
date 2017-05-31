dba151dec278e1337f4e36ec779aa2f0b88f52e5
jarehec <bone1198@gmail.com>
2017-05-31 20:28:12 +0000

    switch free conditions

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+#include <stdio.h>
 /**
  * hash_table_create - Creates an empty hash table
  * @size: size of the hash table node
@@ -9,14 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
+	printf("%lu\n", size);
 	table = malloc(sizeof(hash_table_t));
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table || !table->array || size < 1)
+	if (!table || !table->array || size == 0)
 	{
-		if (table)
-			free(table);
 		if (table->array)
 			free(table->array);
+		if (table)
+			free(table);
 		return (NULL);
 	}
 	table->size = size;
