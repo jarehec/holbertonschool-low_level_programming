43ea9877460fb1bf3319b4af6c1c38e86d0d1863
jarehec <bone1198@gmail.com>
2017-05-31 22:31:56 +0000

    TEST stuarts code

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,25 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates an empty hash table
- * @size: size of the hash table node
- * Return: pointer to hash_table
+ * hash_table_create - creates a hash table
+ * @size: size of the hashtable (2D array)
+ * Return: pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *ht;
 
 	if (size == 0)
 		return (NULL);
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-/*		free(table);*/
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
 		return (NULL);
-	}
-	return (table);
+
+
+	return (ht);
+
 }
