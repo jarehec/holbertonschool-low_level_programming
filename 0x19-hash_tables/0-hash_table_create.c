0cdf6b000feeb803425707a02c6f02984731255b
jarehec <bone1198@gmail.com>
2017-05-31 22:10:19 +0000

    remove free

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,8 +15,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = calloc(size, sizeof(hash_node_t *));
 	if (!table->array)
 	{
-		free(table);
-		return (NULL);
+/*		free(table);
+*/		return (NULL);
 	}
 	table->size = size;
 	return (table);
