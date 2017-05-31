20dc41c1c291d26af8dfbfacfd0970c7c398f645
jarehec <bone1198@gmail.com>
2017-05-31 20:39:04 +0000

    TEST: allow 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,8 +9,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
