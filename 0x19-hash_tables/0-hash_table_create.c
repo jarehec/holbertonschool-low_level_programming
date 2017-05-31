259db9970196ce1daaa39615acbc9abb81b0273d
jarehec <bone1198@gmail.com>
2017-05-31 18:33:28 +0000

    check for 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,9 +9,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
+	if (size == 0)
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table || !table->array || size == 0)
+	if (!table || !table->array)
 	{
 		if (table)
 			free(table);
