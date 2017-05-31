ef02f3fcce3f3021758a810fd31e94c57927cf16
jarehec <bone1198@gmail.com>
2017-05-31 22:20:27 +0000

    removed null init

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,16 +7,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 
-	table = calloc(1, sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->array = calloc(size, sizeof(hash_node_t *));
 	if (!table->array)
 	{
-/*		free(table);
-*/		return (NULL);
+		free(table);
+		return (NULL);
 	}
 	table->size = size;
 	return (table);
