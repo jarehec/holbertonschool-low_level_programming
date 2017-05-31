1752fa109be499592576dec11750cba895f7502a
jarehec <bone1198@gmail.com>
2017-05-31 18:00:27 +0000

    free before return

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,7 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table || !table->array)
+	{
+		if (table)
+			free(table);
+		if (table->array)
+			free(table);
 		return (NULL);
+	}
 	table->size = size;
 	return (table);
 }
