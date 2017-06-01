35ba69e99f0d29240307793b14807cc648a28346
jarehec <bone1198@gmail.com>
2017-06-01 01:54:36 +0000

    removed helper function

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *temp;
 	unsigned long int index;
 
 	if (!ht || !key)
@@ -25,38 +25,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = node;
 	else
 	{
-		if (update_key(ht->array[index], key, value) == 0)
+		for (temp = ht->array[index]; temp ; temp = temp->next)
 		{
-			free(node->value);
-			free(node->key);
-			free(node);
-			return (1);
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(node->value);
+				free(node->key);
+				free(node);
+				free(temp->value);
+				temp->value = strdup(value);
+				return (0);
+			}
 		}
 		ht->array[index]->next = node;
 	}
 	return (1);
-}
-
-/**
- * update_key - replaces idx->key with str if they match
- * @idx: pointer to a hash node
- * @str: key to compare
- * @val: updated value
- * Return: 0 if key is replaced, else -1
- */
-int update_key(hash_node_t *node, const char *str, const char *val)
-{
-	hash_node_t *temp = node;
-
-	while(temp)
-	{
-		if (strcmp(temp->key, str) == 0)
-		{
-			free(temp->value);
-			temp->key = strdup(val);
-			return (0);
-		}
-		temp = temp->next;
-	}
-	return (-1);
 }
