07b1d3198622b216501107651a0751c6810c3a0d
jarehec <bone1198@gmail.com>
2017-06-05 03:49:41 +0000

    forgot to initialize temp

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -8,7 +8,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *temp = NULL;
+	hash_node_t *temp;
 	unsigned long int index;
 
 	if (!ht || !key)
@@ -16,6 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index(((const unsigned char *)key), ht->size);
 	if (!ht->array[index])
 		return (NULL);
+	temp = ht->array[index];
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
