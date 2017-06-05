4ab813dd327a45db23155bb29b38ae4d4491ea2a
jarehec <bone1198@gmail.com>
2017-06-05 03:46:44 +0000

    initial commit

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ * Return: value associated with key, else NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int index;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index(((const unsigned char *)key), ht->size);
+	if (!ht->array[index])
+		return (NULL);
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+	}
+	return (NULL);
+}
