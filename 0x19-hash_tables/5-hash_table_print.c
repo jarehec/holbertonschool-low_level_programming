f7da84fdafc48d69d096c427a2f17861ea58b419
jarehec <bone1198@gmail.com>
2017-06-05 21:43:18 +0000

    fixed output

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -13,16 +13,18 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		for (idx = 0, ht_len = 0; idx < ht->size; idx++)
 			if (ht->array[idx])
-				ht_len = idx;
-		printf("%u\n", ht_len);
+				ht_len++;
+		printf("%d\n", ht_len);
 		putchar('{');
 		for (idx = 0; idx < ht->size; idx++)
 		{
 			temp = ht->array[idx];
+			if (temp)
+				ht_len--;
 			while (temp)
 			{
 				printf("'%s': '%s'", temp->key, temp->value);
-				if (temp->next || idx < ht_len)
+				if (ht_len > 0 || temp->next)
 					printf(", ");
 				temp = temp->next;
 			}
