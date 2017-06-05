8cd9008b5ae2b0d7761f2fb85bb6fb2b40f4223c
jarehec <bone1198@gmail.com>
2017-06-05 21:03:29 +0000

    initial commit

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the data in a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned int idx = 0, ht_len;
+
+	if (ht->array)
+	{
+		for (idx = 0, ht_len = 0; idx < ht->size; idx++)
+			if (ht->array[idx])
+				ht_len = idx;
+		printf("%u\n", ht_len);
+		putchar('{');
+		for (idx = 0; idx < ht->size; idx++)
+		{
+			temp = ht->array[idx];
+			while (temp)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				if (idx < ht_len)
+					printf(", ");
+				temp = temp->next;
+			}
+		}
+		putchar('}');
+		putchar('\n');
+	}
+}
