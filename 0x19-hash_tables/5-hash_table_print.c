ebe974e879b65b968547a875320663a5dc946dcd
jarehec <bone1198@gmail.com>
2017-06-05 22:58:55 +0000

    revert

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -9,29 +9,26 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *temp;
 	unsigned int idx = 0, ht_len;
 
-	if (ht && ht->array && ht->size > 0)
+	if (ht && ht->array)
 	{
 		for (idx = 0, ht_len = 0; idx < ht->size; idx++)
 			if (ht->array[idx])
 				ht_len++;
-		if (ht_len > 0)
+		putchar('{');
+		for (idx = 0; idx < ht->size; idx++)
 		{
-			putchar('{');
-			for (idx = 0; idx < ht->size; idx++)
+			temp = ht->array[idx];
+			if (temp)
+				ht_len--;
+			while (temp)
 			{
-				temp = ht->array[idx];
-				if (temp)
-					ht_len--;
-				while (temp)
-				{
-					printf("'%s': '%s'", temp->key, temp->value);
-					if (ht_len > 0 || temp->next)
-							printf(", ");
-					temp = temp->next;
-				}
+				printf("'%s': '%s'", temp->key, temp->value);
+				if (ht_len > 0 || temp->next)
+					printf(", ");
+				temp = temp->next;
 			}
-			putchar('}');
 		}
+		putchar('}');
 	}
 	putchar('\n');
 }
