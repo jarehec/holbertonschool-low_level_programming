31898b4c2856a002172453276e9c83c082d30df0
jarehec <bone1198@gmail.com>
2017-06-05 23:11:24 +0000

    fixed output

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -14,21 +14,24 @@ void hash_table_print(const hash_table_t *ht)
 		for (idx = 0, ht_len = 0; idx < ht->size; idx++)
 			if (ht->array[idx])
 				ht_len++;
-		putchar('{');
-		for (idx = 0; idx < ht->size; idx++)
+		if (ht_len > 0)
 		{
-			temp = ht->array[idx];
-			if (temp)
-				ht_len--;
-			while (temp)
+			putchar('{');
+			for (idx = 0; idx < ht->size; idx++)
 			{
-				printf("'%s': '%s'", temp->key, temp->value);
-				if (ht_len > 0 || temp->next)
-					printf(", ");
-				temp = temp->next;
+				temp = ht->array[idx];
+				if (temp)
+					ht_len--;
+				while (temp)
+				{
+					printf("'%s': '%s'", temp->key, temp->value);
+					if (ht_len > 0 || temp->next)
+						printf(", ");
+					temp = temp->next;
+				}
 			}
+			putchar('}');
 		}
-		putchar('}');
 	}
 	putchar('\n');
 }
