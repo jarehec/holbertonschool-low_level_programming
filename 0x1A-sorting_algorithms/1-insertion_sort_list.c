7f3b6ea1845c27c0d2c71a10218c7d80910ca1d4
jarehec <bone1198@gmail.com>
2017-06-19 22:51:16 +0000

    added insertion sort

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a list in ascending order using insertion sort
+ * @list: list to sort
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list;
+	listint_t *ptr = NULL;
+
+	if (!(list || head || head->next))
+		return;
+	for (head = *list; head; head = head->next)
+	{
+		for (ptr = head->prev; ptr; ptr = ptr->prev)
+		{
+			if (head->n < ptr->n)
+			{
+				head->prev->next = head->next;
+				if (head->next)
+					head->next->prev = head->prev;
+				head->next = ptr;
+				if (ptr->prev)
+				{
+					head->prev = ptr->prev;
+					ptr->prev->next = head;
+					ptr->prev = head;
+				}
+				else
+				{
+					head->prev = NULL;
+					head->next = *list;
+					(*list)->prev = head;
+					*list = head;
+				}
+				print_list(*list);
+			}
+		}
+	}
+}
