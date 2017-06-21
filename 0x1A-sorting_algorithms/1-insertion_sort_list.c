d4cd4b8331db5f606aef62c0dc7b98cf5a9e314f
Stuart Kuredjian <108@holbertonschool.com>
2017-06-21 00:35:34 +0000

    1-insertion_sort.c: redo, initial commit

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -8,45 +8,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *prv, *nxt;
+	listint_t *pn, *walk, *nn;
 
-	if (list == NULL || *list == NULL || node_count(list) < 2)
+	walk = *list;
+	walk = walk->next;
+	pn = walk->prev;
+	nn = walk->next;
+	if (list == NULL || *lsit == NULL || count_nodes(*list) < 2)
 		return;
-
-	temp = (*list)->next;
-	nxt = temp->next; prv = temp->prev;
-	while (temp)
+	while (walk)
 	{
-		if (temp->n < prv->n)
+		if (walk->n < pn->n)
 		{
-			while (prv && temp->n < prv->n)
+			while (pn != NULL && walk->n < pn->n)
 			{
-				/* next */
-				if (nxt)
-					nxt->prev = prv;
+				if (nn)
+					nn->prev = pn;
+				walk->next = pn;
+				walk->prev = pn->prev;
+				pn->next = nn;
+				if (pn->prev != NULL)
+					pn->prev->next = walk;
 
-				/* temp */
-				temp->next = prv;
-				temp->prev = prv->prev;
-
-				/* prev */
-				if (prv->prev)
-					prv->prev->next = temp;
-				prv->prev = temp;
-				prv->next = nxt;
-
-				nxt = prv;
-				prv = temp->prev;
-				if ((*list)->prev)
+				nn = pn;
+				pn = walk->prev;
+				if ((*list)->prev != NULL)
 					*list = (*list)->prev;
 				print_list(*list);
 			}
 		}
-		temp = nxt;
-		if (temp != NULL)
-		{
-			nxt = temp->next; prv = temp->prev;
-		}
+		walk = walk->next;
+		nn = walk->next;
+		pn = walk->prev;
 	}
 }
 
@@ -55,18 +48,57 @@ void insertion_sort_list(listint_t **list)
  * @list: the list to measure
  * Return: the number of nodes in the list
  */
-
 int node_count(listint_t **list)
 {
 	listint_t *ptr;
 	int count = 0;
 
 	ptr = *list;
-
 	while (ptr)
 	{
 		ptr = ptr->next;
 		count++;
 	}
 	return (count);
+}
+
+/**
+ * print_nodes - debug tool that prints the current n value for pn, walk and nn
+ * @pn: ptr to the initial node before walk
+ * @walk: ptr to node in focus
+ * @nn: ptr to ther initial node after walk
+ * @expand: option to show the ->prev and ->next for each node
+ * Return: void
+ */
+void print_nodes(listint_t *pn, listint_t *walk, listint_t *nn, int expand)
+{
+	printf("pn");
+	if (pn == NULL)
+		printf(": NULL");
+	else
+	{
+		printf("->n: %d", pn->n);
+		if (expand == 1)
+		{
+			printf("\n");
+			printf("\t - pn->prev->n %d\n", pn->prev->n);
+			printf("\t - pn->next->n %d\n", pn->next->n);
+		}
+	}
+
+	printf("\n");
+
+	printf("walk");
+	if (walk == NULL)
+		printf(": NULL");
+	else
+		printf("->n: %d", walk->n);
+	printf("\n");
+
+		printf("nn");
+	if (nn == NULL)
+		printf(": NULL");
+	else
+		printf("->n: %d", nn->n);
+	printf("\n");
 }
