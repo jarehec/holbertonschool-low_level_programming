d50b672bbf143d68b4f4fa62dc43ae1a2fda404e
jarehec <bone1198@gmail.com>
2017-07-25 00:33:29 +0000

    reduced line count

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -8,10 +8,5 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node)
-	{
-		if (!node->left && !node->right)
-			return (1);
-	}
-	return (0);
+	return (node->left || node->right ? 0 : 1);
 }
