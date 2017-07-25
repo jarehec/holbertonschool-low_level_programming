dd1303403f8ca1c957b23cce2972db3eaa1b784d
jarehec <bone1198@gmail.com>
2017-07-25 22:40:07 +0000

    initial commit

---
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: binary tree
+ *
+ * Return: number of nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			count += binary_tree_size(tree->left);
+		if (tree->right)
+			count += binary_tree_size(tree->right);
+		count++;
+	}
+	return (count);
+}
