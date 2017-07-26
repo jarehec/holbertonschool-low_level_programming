ea6f8c87a95500c46e54b4bd5bd5b585530388ee
jarehec <bone1198@gmail.com>
2017-07-26 21:15:32 +0000

    initial commit

---
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: binary tree
+ *
+ * Return: 1 if full, else 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int is_full = 0;
+
+	if (tree && tree->left && tree->right)
+	{
+		is_full = 1;
+		is_full = binary_tree_is_full(tree->left);
+		is_full = binary_tree_is_full(tree->right);
+	}
+	else if (tree && !tree->left && !tree->right)
+		is_full = 1;
+	return (is_full);
+}
