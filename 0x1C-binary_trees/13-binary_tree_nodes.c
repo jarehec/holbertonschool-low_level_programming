778ce3893fc721d4d66fc6382d97165a4d2f6162
jarehec <bone1198@gmail.com>
2017-07-26 18:56:18 +0000

    initial commit

---
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least one child
+ * @binary tree
+ * Return: count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree)
+	{
+		if (tree->left || tree->right)
+			count++;
+		if (tree->left)
+			count += binary_tree_nodes(tree->left);
+		if (tree->right)
+			count += binary_tree_nodes(tree->right);
+	}
+	return (count);
+}
