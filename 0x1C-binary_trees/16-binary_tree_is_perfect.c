015961ef7e64408210536e416e2b8d22d5080c50
jarehec <bone1198@gmail.com>
2017-07-27 20:42:32 +0000

    test

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -8,61 +8,19 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t size;
+	size_t left, right;
 
 	if (tree)
 	{
-		size = binary_tree_size(tree);
-		if ((2 * (binary_tree_leaves(tree)) - 1) == size)
+		if (binary_tree_balance(tree) == 0)
 		{
-			if (binary_tree_balance(tree) == 0)
+			left = binary_tree_is_perfect(tree->left);
+			right = binary_tree_is_perfect(tree->right);
+			if (left == right)
 				return (1);
 		}
 	}
 	return (0);
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: binary tree
- *
- * Return: number of nodes
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t count = 0;
-
-	if (tree)
-	{
-		if (tree->left)
-			count += binary_tree_size(tree->left);
-		if (tree->right)
-			count += binary_tree_size(tree->right);
-		count++;
-	}
-	return (count);
-}
-
-/**
- * binary_tree_leaves - counts the number of leaves in a binary tree
- * @tree: binary tree
- *
- * Return: number of leaves
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	size_t count = 0;
-
-	if (tree)
-	{
-		if (!tree->left && !tree->right)
-			count++;
-		if (tree->left)
-			count += binary_tree_leaves(tree->left);
-		if (tree->right)
-			count += binary_tree_leaves(tree->right);
-	}
-	return (count);
 }
 
 /**
