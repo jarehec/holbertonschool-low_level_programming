4f5c7104d8084cba831220b2bd91da42603f8e69
jarehec <bone1198@gmail.com>
2017-07-27 21:07:50 +0000

    initial commit

---
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: binary tree node
+ *
+ * Return: node sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node && node->parent)
+	{
+		if (node->parent->left == node)
+			return (node->parent->right);
+		else
+			return(node->parent->left);
+	}
+	return (NULL);
+}
