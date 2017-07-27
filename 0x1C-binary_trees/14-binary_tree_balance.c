abfc168c6b8b1a66ee1a1e873bc9e24c1c494896
jarehec <bone1198@gmail.com>
2017-07-27 06:09:10 +0000

    add one

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -13,9 +13,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left)
-			left = binary_tree_height(tree->left);
+			left = binary_tree_height(tree->left) + 1;
 		if (tree->right)
-			right = binary_tree_height(tree->right);
+			right = binary_tree_height(tree->right) + 1;
 	}
 	return (left - right);
 }
