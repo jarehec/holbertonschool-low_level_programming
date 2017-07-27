e2c52177478165370598be0a52a4740eedcc0e6a
jarehec <bone1198@gmail.com>
2017-07-27 05:59:36 +0000

    using helper function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -8,20 +8,40 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int right = 0, left = 0;
+	int left = 0, right = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			left = binary_tree_height(tree->left);
+		if (tree->right)
+			right = binary_tree_height(tree->right);
+	}
+	return (left - right);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ *
+ * Return: height of binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right = 0, left = 0;
 
 	if (tree)
 	{
 		if (tree->left)
 		{
-			left += binary_tree_balance(tree->left);
+			left += binary_tree_height(tree->left);
 			left++;
 		}
 		if (tree->right)
 		{
-			right += binary_tree_balance(tree->right);
+			right += binary_tree_height(tree->right);
 			right++;
 		}
 	}
-	return (left - right);
+	return (right < left ? left : right);
 }
