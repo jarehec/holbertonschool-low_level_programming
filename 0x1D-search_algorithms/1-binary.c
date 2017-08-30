cf1ea2e01001bc6444d187181477823aa0edbdb8
jarehec <bone1198@gmail.com>
2017-08-30 23:37:09 +0000

    print array before displaying result

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -18,13 +18,13 @@ int binary_search(int *array, size_t size, int value)
 				return (left);
 			if (array[right] == value)
 				return (right);
-			if (array[mid] == value)
-				return (mid);
 			print_array(array, left, right < size - 1 ? right + 1 : right);
 			if (array[mid] > value)
 				right = mid - 1;
 			if (array[mid] < value)
 				left = mid + 1;
+			if (array[mid] == value)
+				return (mid);
 			mid = (left + right) / 2;
 		}
 		print_array(array, left, right < size - 1 ? right + 1 : right);
