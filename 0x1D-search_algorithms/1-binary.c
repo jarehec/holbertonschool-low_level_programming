f36ee375dbd405416d3e19821da47787620299a3
jarehec <bone1198@gmail.com>
2017-08-31 02:52:53 +0000

    -m

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 			mid = (left + right) / 2;
 			print_array(array, left, right < size - 1 ? right + 1 : right);
 			if (array[mid] > value)
-				right = mid;
+				right = mid - 1;
 			if (array[mid] < value)
 				left = mid + 1;
 			if (array[mid] == value)
