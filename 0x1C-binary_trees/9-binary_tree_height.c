6dc5c71d82e4502e00b7e2f6c4289643cdcac453
jarehec <bone1198@gmail.com>
2017-10-11 21:37:42 +0000

    reduced line count

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -13,15 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left)
-		{
-			left += binary_tree_height(tree->left);
-			left++;
-		}
+			left = 1 + binary_tree_height(tree->left);
 		if (tree->right)
-		{
-			right += binary_tree_height(tree->right);
-			right++;
-		}
+			right = 1 + binary_tree_height(tree->right);
 	}
 	return (right < left ? left : right);
 }
